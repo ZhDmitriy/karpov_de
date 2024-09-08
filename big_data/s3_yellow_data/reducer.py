f6924ff475c7f04e87534f816a653764b5391c36
Dmitriy Zhdanov <141666797+ZhDmitriy@users.noreply.github.com>
2024-09-08 20:57:57 +0300

    Add files via upload

---
+++ big_data/s3_yellow_data/reducer.py
@@ -0,0 +1,17 @@
+# Выполняем map-reduce задание локально
+
+""" reducer.py """
+
+import pandas as pd
+
+
+def perform_reduce(file_reducer: pd.DataFrame) -> pd.DataFrame:
+    file_reducer = file_reducer[['month', 'payment_type', 'tip_amount']]
+    file_reducer = file_reducer.groupby(['month', 'payment_type']).agg({'tip_amount': 'mean'})
+    file_reducer = file_reducer.rename(columns={"month": "month", "payment_type": "payment_type", "tip_amount": "tips average amount"}).reset_index()
+    file_reducer = file_reducer[(file_reducer['payment_type'] != 'Unknown')]
+    return file_reducer
+
+
+if __name__ == '__main__':
+    perform_reduce(pd.read_csv(r"D:\mapreduce\reducer.csv")).to_csv('result.csv')
